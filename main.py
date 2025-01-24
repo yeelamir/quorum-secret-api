4ad6187e6693e211f28fe9e92d029d24cf9a38b6
yeelamir <yeela.mir@gmain.com>
2025-01-24 10:48:06 +0200

    Added register handler

--- main.py
+++ main.py
@@ -18,14 +18,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+fake_users_db = {}
+
 class User(BaseModel):
     username: str
     password: str
 
 @app.post("/login")
 def login(user: User):
-    if user.username == "yeelam" and user.password == "theQueen":
+    global fake_users_db
+    if user.username in fake_users_db:
         return {"validation": True}
     else:
         return {"validation": False}    
-    
+
+@app.post("/register")
+def register(user: User):
+    global fake_users_db  
+    if user.username in fake_users_db:
+        # Username already exists
+        return {"validation": False, "message": "Username already exists"}
+    else:
+        fake_users_db[user.username] = user.password
+        for key in fake_users_db.keys():
+            print("un: " + key + " ps: " + fake_users_db[key])
+        return {"validation": True, "message": "User registered successfully"}
+
+
